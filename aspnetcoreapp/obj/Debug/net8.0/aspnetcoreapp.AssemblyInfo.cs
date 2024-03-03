203e5df06e9bcc54970628ab95d46ff27bd40c3f
unknown <davedalimocon@gmail.com>
2024-03-03 23:51:20 +0800

    added angular

--- aspnetcoreapp/obj/Debug/net8.0/aspnetcoreapp.AssemblyInfo.cs
+++ aspnetcoreapp/obj/Debug/net8.0/aspnetcoreapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aspnetcoreapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f60c951a513c0781f6f3b9e7b532c656bd81bf51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efbc2c317c59cb12e12987542f1e553a94d0d675")]
 [assembly: System.Reflection.AssemblyProductAttribute("aspnetcoreapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aspnetcoreapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
